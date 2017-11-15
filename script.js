e0d64b6b7442a8fb145e5c34b48f00cf05a2b080
Yasir Ali <33684074+Yasiraliweb@users.noreply.github.com>
2017-11-15 15:41:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var screen = document.getElementById('screen');
+var caption = document.getElementById('caption');
+var info = document.getElementById('info');
+
+var thumbnails = document.getElementById('thumbnails');
+
+function initGallery() {
+	
+	loadImage(0);
+
+	for(var i = 0; i < images.length; i++) {
+		var image = images[i];
+		var img = document.createElement('img');
+		img.src = 'images/thumbs/tn_' + images[i].url;
+		img.setAttribute('width', '170');
+		img.setAttribute('data-index', i);
+		img.addEventListener('click', changeImage);
+		thumbnails.appendChild(img);
+	}
+};
+
+function changeImage(event) {
+	var target = event.currentTarget;
+	var index = target.getAttribute('data-index');
+	loadImage(index);
+};
+
+function loadImage(index) {
+	var image = images[index];
+	screen.src = 'images/' + image.url;
+	caption.textContent = image.caption;
+	info.textContent = image.info;
+};
+
+initGallery();
